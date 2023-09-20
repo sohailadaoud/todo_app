38c63fd527958b281496ebc8fb87df91e308cab6
Sohaila Daoud <63005434+sohailadaoud@users.noreply.github.com>
2023-09-20 19:21:13 +0300

    appBar titles

--- lib/Home/home_screen.dart
+++ lib/Home/home_screen.dart
@@ -14,6 +14,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int selectedIndex = 0;
+  final List<String> appBarTitles = ['To_Do List', 'Settings'];
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'To-Do List',
+          appBarTitles[selectedIndex],
           style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
